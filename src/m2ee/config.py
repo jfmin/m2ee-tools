251bd87acede3bcc0e3ed10138e3dfc5a61de2b7
Jouke Waleson <jouke.waleson@mendix.com>
2016-03-22 15:57:16 +0100

    allow MXURL and MXPASSWORD env vars to be used

--- src/m2ee/config.py
+++ src/m2ee/config.py
@@ -6,23 +6,34 @@
 #
 
 import getpass
+import os
+from log import logger
 
 
 class M2EEConfig:
 
     def __init__(self):
-        self.url = raw_input('Give url, i.e.: https://my.app.com\n').strip()
+        if os.getenv('MXURL', None):
+            self.url = os.getenv('MXURL')
+        else:
+            self.url = raw_input(
+                'Give url, i.e.: https://my.app.com\n'
+            ).strip()
+
         if self.url.endswith('/'):
             self.url = self.url[:-1]
         if not self.url.startswith('http'):
             self.url = 'https://' + self.url
-        print 'using url', self.url
+        logger.info('using url %s' % self.url)
 
-        self.password = getpass.getpass(
-            'Give admin password, from the ADMIN_PASSWORD env var'
-        ).strip()
+        if os.getenv('MXPASSWORD', None):
+            self.password = os.getenv('MXPASSWORD')
+        else:
+            self.password = getpass.getpass(
+                'Give admin password, from the ADMIN_PASSWORD env var'
+            ).strip()
 
         self.url = self.url + '/_mxadmin/'
 
-        print ''
-        print 'try the help command to get started'
+        logger.info('')
+        logger.info('try the help command to get started')
